3a8c8b102b58054be03e137065cbc4c5cce75ef0
SathishKumar Rajendran <90351356+sathishkumar130913@users.noreply.github.com>
2021-11-02 18:39:23 +0000

    Added Test and Core Logic Projects

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -11,10 +11,11 @@ namespace TestJenkinsApp.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
+        RulesApp objru = new RulesApp();
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+            int a = objru.Addlogic(5, 5);
         }
 
         public void OnGet()
---
+++ RulesApp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Jenkins.CoreApp;
+
+namespace TestJenkinsApp
+{    
+    public class RulesApp
+    {
+         RuleLogic objRulelogic = new RuleLogic();
+         public int Addlogic(int a, int b)
+         {
+           return objRulelogic.Sumlogic(5, 5);
+         }
+    }
+}
